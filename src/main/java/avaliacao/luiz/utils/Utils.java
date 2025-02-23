e0429be1fc10ddf468643fa775e3b538af843afe
Luiz Vinicius <viniciusadr244@gmail.com>
2025-02-23 13:03:34 -0300

    feat: new input methods

--- src/main/java/avaliacao/luiz/utils/Utils.java
+++ src/main/java/avaliacao/luiz/utils/Utils.java
@@ -1,25 +1,50 @@
 package avaliacao.luiz.utils;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Utils implements AutoCloseable {
-    private Scanner scan;
+    private final Scanner scan;
 
     public Utils() {
         this.scan = new Scanner(System.in);
     }
 
     public String lerString(String msg, String errorMessage) {
-        var scan = System.console();
         while (true) {
             System.out.print(msg);
-            var palavra = scan.readLine().strip().toLowerCase();
+            var palavra = this.scan.nextLine().strip().toLowerCase();
             if (!Pattern.matches("^[a-zÀ-ÿ\s]+$", palavra)) {
                 System.out.println(errorMessage + "\n");
                 continue;
             }
             return palavra;
+        }
+    }
+
+    public String lerOnzeDigitos(String msg, String errorMessage) {
+        while (true) {
+            System.out.print(msg);
+            var numero = this.scan.nextLine().strip();
+            if (!Pattern.matches("^\\d{11}$", numero)) {
+                System.out.println(errorMessage + "\n");
+                continue;
+            }
+            return numero;
+        }
+    }
+
+    public String lerCnpj(String msg, String errorMessage) {
+        while (true) {
+            System.out.print(msg);
+            var numero = this.scan.nextLine().strip();
+            if (!Pattern.matches("^\\d{14}$", numero)) {
+                System.out.println(errorMessage + "\n");
+                continue;
+            }
+            return numero;
         }
     }
 
@@ -54,9 +79,26 @@ public class Utils implements AutoCloseable {
                 if (num < min || num > max) {
                     throw new IllegalArgumentException(errorMessage + "\n");
                 }
-                return num;
+                return num - 1;
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public LocalDate lerData(String msg) {
+        while (true) {
+            System.out.print(msg);
+            String data = this.scan.nextLine().strip();
+            if (!Pattern.matches("^\\d{2}/\\d{2}/\\d{4}$", data)) {
+                System.out.println("Data no formato inválido\n");
+                continue;
+            }
+            try {
+                var arrayData = data.split("/");
+                return LocalDate.parse(String.format("%s-%s-%s", arrayData[2], arrayData[1], arrayData[0]));
+            } catch (DateTimeParseException e) {
+                System.out.println("Data inválida");
             }
         }
     }
